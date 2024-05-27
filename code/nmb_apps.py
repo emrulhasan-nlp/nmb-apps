a8676b3114b306281b10d785f226282d379a7f0d
Emrul <emrul.hasan@vectorinstitute.ai>
2024-05-27 14:29:05 -0400

    code updated

--- code/nmb_apps.py
+++ code/nmb_apps.py
@@ -1,4 +1,3 @@
-
 from PIL import Image
 import streamlit as st
 import os,inspect
@@ -15,16 +14,14 @@ images=os.listdir(data_directory+folder_dir[1])
 
 for i, (img, cont) in enumerate(zip(images,contents)):
 
-    #if img[-7:-3]!=cont[-7:-3]:
-    image_path=data_directory+f'/{folder_dir[1]}/{img}'
-    
-    image=Image.open(image_path)
-    st.image(image, caption=img)
-    
-    content_path=data_directory+f'/{folder_dir[0]}/{cont}'
-    with open(content_path, 'r') as f:
-        text=f.read()
-        st.text_area(label=cont, value=text, height=200)
+    if img[-7:-3]!=cont[-7:-3]:
+        image_path=data_directory+f'/{folder_dir[1]}/{img}'
+        
+        image=Image.open(image_path)
+        st.image(image, caption=img)
 
-# if __name__ == '__main__':
-#     st.run()
+        content_path=data_directory+f'/{folder_dir[0]}/{cont}'
+        with open(content_path, 'r') as f:
+            text=f.read()
+
+            st.text_area(label=cont, value=text, height=200)
