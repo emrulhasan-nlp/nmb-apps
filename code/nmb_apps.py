cc9be202152e82933c38ef1f17085e2656fe1950
Emrul <emrul.hasan@vectorinstitute.ai>
2024-05-27 10:46:17 -0400

    code updated

---
+++ code/nmb_apps.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import streamlit as st
+print("hello")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+print(parent_dir)
