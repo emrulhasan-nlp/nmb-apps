f052517d2f8336abc86bd1ddd8479287cdfc0ff1
Emrul <emrul.hasan@vectorinstitute.ai>
2024-05-27 13:54:03 -0400

    code upadeted to fix matching

--- code/nmb_apps.py
+++ code/nmb_apps.py
@@ -13,19 +13,19 @@ contents=os.listdir(data_directory+folder_dir[0])
 images=os.listdir(data_directory+folder_dir[1])
 
 
-for img, cont in zip(images,contents):
-    
-    image_path=data_directory+f'/{folder_dir[1]}/{img}'
-    
-    image=Image.open(image_path)
-    st.image(image, caption=img)
+for i, (img, cont) in enumerate(zip(images,contents)):
 
+    if img[-7:-3]!=cont[-7:-3]:
+        image_path=data_directory+f'/{folder_dir[1]}/{img}'
+        
+        image=Image.open(image_path)
+        st.image(image, caption=img)
+        
+        content_path=data_directory+f'/{folder_dir[0]}/{cont}'
+        with open(content_path, 'rb') as f:
+            text=f.read()
 
-    content_path=data_directory+f'/{folder_dir[0]}/{cont}'
-    with open(content_path, 'rb') as f:
-        text=f.read()
-
-        st.text_area(label=cont, value=text, height=200)
+            st.text_area(label=cont, value=text, height=200)
 
 # if __name__ == '__main__':
 #     st.run()
