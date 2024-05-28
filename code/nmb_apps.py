3b0c53b0cc193f433f71acbaada0d263b59b241b
Emrul <emrul.hasan@vectorinstitute.ai>
2024-05-28 09:23:03 -0400

    data and codes are updated

--- code/nmb_apps.py
+++ code/nmb_apps.py
@@ -19,6 +19,8 @@ for datafolder in folder_dir:
 
     for i, (img, cont) in enumerate(zip(images,contents)):
 
+        #st.title("Shaina's Portion")
+
         image_path=data_directory+f'{datafolder}/images/{img}'
         content_path=data_directory+f'{datafolder}/content/{cont}'
 
@@ -28,8 +30,18 @@ for datafolder in folder_dir:
         clean_text=read_and_print_cleaned_file(content_path)
         st.text_area(label=cont, value=clean_text, height=400)
 
+        if i==0:
+            st.title("Shaina's portion")
         if i==40:
-            st.title("Next batch started")
+            st.title("Veronica's portion")
+        if i==80:
+            st.title("Ananayna's portion")
+        if i==120:
+            st.title("Mark's portion")
+        if i==160:
+            st.title("Marcelo's portion")
         if i==200:
+            st.title("Marcelo's portion")
+        if i>200:
             st.title("200 ends here")
             break
